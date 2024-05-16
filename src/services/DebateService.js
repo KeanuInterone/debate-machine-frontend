caad6047406a15f7cce46513728f8f2ffd75fc75
Keanu Interone <keanu.interone@gmail.com>
2024-05-16 12:00:03 +1000

    Hooked up to backend

--- src/services/DebateService.js
+++ src/services/DebateService.js
@@ -8,29 +8,19 @@ const DebateService = {
             who_starts: whoStarts
         }
 
-        // const response = await fetch('https://ovtuu968q1.execute-api.us-east-2.amazonaws.com/v1/ssd-chatbot', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(postBody)
-        // });
+        const response = await fetch('https://n09b4i2wd4.execute-api.ap-southeast-4.amazonaws.com/v1/debate-machine', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postBody)
+        });
 
-        // const responseBody = await response.json();
-
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-
-        const responseBody = {
-            deb_id: "12345",
-            first_message: {
-                name: "Affirmative",
-                message: "I am the affirmative side. and I aggree with the topic",
-            }
-        }
+        const responseBody = await response.json();
 
         debId = responseBody.deb_id;
 
-        return responseBody.first_message;
+        return responseBody.response;
     },
 
     advanceDebate: async () => {
@@ -38,26 +28,17 @@ const DebateService = {
             deb_id: debId
         }
 
-        // const response = await fetch('https://ovtuu968q1.execute-api.us-east-2.amazonaws.com/v1/ssd-chatbot', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(postBody)
-        // });
+        const response = await fetch('https://n09b4i2wd4.execute-api.ap-southeast-4.amazonaws.com/v1/debate-machine', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postBody)
+        });
 
-        // const responseBody = await response.json();
+        const responseBody = await response.json();
 
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-
-        const responseBody = {
-            next_message: {
-                name: "Negative",
-                message: "I am the negative side. and I dissagree with the topic",
-            }
-        }
-
-        return responseBody.next_message;
+        return responseBody.response;
     }
 
 };
